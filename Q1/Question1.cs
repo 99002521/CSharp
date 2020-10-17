9aebaf5aae138875155961156b2562e480932284
99002521 <72495750+99002521@users.noreply.github.com>
2020-10-17 14:02:28 +0530

    Question1
    Array

---
+++ Q1/Question1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssessmentRetry
+{
+    class SampleArray
+    {
+        static void DisplayArray(string[] arr) => Console.WriteLine(string.Join(" ", arr));
+
+        
+        static void ChangeArray(string[] arr) => Array.Reverse(arr);
+
+
+        static void Main()
+        {
+            string[] weekDays = new string[7];
+
+            Console.WriteLine("Enter the days of a week in order:");
+            for (int day = 0; day < 7; day++)
+            {
+                Console.WriteLine("Enter day:");
+                weekDays[day] = Console.ReadLine();
+                   
+            }
+            
+            Console.WriteLine("Before");
+            DisplayArray(weekDays);
+            Console.WriteLine();
+
+            
+            ChangeArray(weekDays);
+            
+            Console.WriteLine("After:");
+            DisplayArray(weekDays);
+            Console.WriteLine();
+
+         
+        }
+    }
+}
